123a2cba5404462eb5c3f6df0f92f6b2bb1c4cae
Siddhata <siddhata.s.patil@gmail.com>
2017-09-16 15:01:53 -0700

    Parsing Data to JSON models

--- app/src/main/java/com/codepath/siddhatapatil/flicksassgn1/MainActivity.java
+++ app/src/main/java/com/codepath/siddhatapatil/flicksassgn1/MainActivity.java
@@ -2,13 +2,22 @@ package com.codepath.siddhatapatil.flicksassgn1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.codepath.siddhatapatil.flicksassgn1.models.Movie;
 import com.loopj.android.http.*;
 
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONArray;
+
+
+import java.util.ArrayList;
 
 import cz.msebera.android.httpclient.Header;
+
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<Movie> movies;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +27,14 @@ public class MainActivity extends AppCompatActivity {
         client.get(url, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                super.onSuccess(statusCode, headers, response);
+                //super.onSuccess(statusCode, headers, response);
+                JSONArray moviejasonResults = null;
+                try {
+                    moviejasonResults = response.getJSONArray("results");
+                    movies = Movie.fromJSONArray(moviejasonResults);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
 
             @Override
